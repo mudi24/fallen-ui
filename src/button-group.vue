1bd7f8d2a29297757d4bac7fdae7f358c26345c2
Irelia <yaokai729@outlook.com>
2020-01-24 21:58:46 +0800

    完成button-group组件

---
+++ src/button-group.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="f-button-group">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang='scss' scoped>
+.f-button-group {
+  display: inline-flex;
+  vertical-align: middle;
+  > .f-button {
+    border-radius: 0;
+    margin-left: -1px;
+    &:first-child {
+      border-top-left-radius: var(--border-radius);
+      border-bottom-left-radius: var(--border-radius);
+    }
+    &:last-child {
+      border-top-right-radius: var(--border-radius);
+      border-bottom-right-radius: var(--border-radius);
+    }
+    &:hover {
+      border: 1px solid red;
+      position: relative;
+      z-index: 1;
+    }
+  }
+}
+</style>
