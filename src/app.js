9e21a65a246b981be32687f4e09d2115a3141b6a
Irelia <yaokai729@outlook.com>
2020-01-25 20:49:32 +0800

    使用 chai-spies 中的 chai.spy 监听 click 回调函数

--- src/app.js
+++ src/app.js
@@ -1,83 +1,85 @@
-import Vue from 'vue';
-import Button from './button.vue';
-import Icon from './icon.vue';
-import ButtonGroup from './button-group.vue'
+import Vue from "vue";
+import Button from "./button.vue";
+import Icon from "./icon.vue";
+import ButtonGroup from "./button-group.vue";
 
-Vue.component('f-button', Button)
-Vue.component('f-icon', Icon)
-Vue.component('f-button-group', ButtonGroup)
+Vue.component("f-button", Button);
+Vue.component("f-icon", Icon);
+Vue.component("f-button-group", ButtonGroup);
 
 new Vue({
-  el: '#app',
+  el: "#app",
   data: {
     loading1: true,
     loading2: false,
     loading3: false
   }
-})
+});
 
+import chai from "chai";
+import spies from "chai-spies";
+chai.use(spies);
 
-import chai from 'chai'
-
-const expect = chai.expect
+const expect = chai.expect;
 // 单元测试
 {
-  const Constructor = Vue.extend(Button)
-  const vm = new Constructor({
-    propsData: {
-      icon: 'settings'
-    }
-  })
-  vm.$mount()
-  let useElement = vm.$el.querySelector('use')
-  let href = useElement.getAttribute('xlink:href')
-  expect(href).to.eq('#i-settings')
-  vm.$el.remove()
-  vm.$destroy()
-} {
-  const Constructor = Vue.extend(Button)
-  const vm = new Constructor({
-    propsData: {
-      icon: 'settings',
-      loading: true
-    }
-  })
-  vm.$mount() // 放在内存中
-  let useElement = vm.$el.querySelector('use')
-  let href = useElement.getAttribute('xlink:href')
-  expect(href).to.eq('#i-loading')
-  vm.$el.remove()
-  vm.$destroy()
-} {
-  const div = document.createElement('div')
-  document.body.appendChild(div)
-  const Constructor = Vue.extend(Button)
-  const vm = new Constructor({
-    propsData: {
-      icon: 'settings',
-    }
-  })
-  vm.$mount(div) // 放在页面中，否则css不会添加到元素上
-  let svg = vm.$el.querySelector('svg')
-  let {
-    order
-  } = window.getComputedStyle(svg)
-  expect(order).to.eq('1')
-  vm.$el.remove()
-  vm.$destroy()
-}
-// mock
-{
-  const Constructor = Vue.extend(Button)
+  const Constructor = Vue.extend(Button);
   const vm = new Constructor({
     propsData: {
       icon: "settings"
     }
-  })
-  vm.$mount()
-  vm.$on('click', function () {
-    expect(1).to.eq(1)
-  })
-  let button = vm.$el
-  button.click()
+  });
+  vm.$mount();
+  let useElement = vm.$el.querySelector("use");
+  let href = useElement.getAttribute("xlink:href");
+  expect(href).to.eq("#i-settings");
+  vm.$el.remove();
+  vm.$destroy();
+}
+{
+  const Constructor = Vue.extend(Button);
+  const vm = new Constructor({
+    propsData: {
+      icon: "settings",
+      loading: true
+    }
+  });
+  vm.$mount(); // 放在内存中
+  let useElement = vm.$el.querySelector("use");
+  let href = useElement.getAttribute("xlink:href");
+  expect(href).to.eq("#i-loading");
+  vm.$el.remove();
+  vm.$destroy();
+}
+{
+  const div = document.createElement("div");
+  document.body.appendChild(div);
+  const Constructor = Vue.extend(Button);
+  const vm = new Constructor({
+    propsData: {
+      icon: "settings"
+    }
+  });
+  vm.$mount(div); // 放在页面中，否则css不会添加到元素上
+  let svg = vm.$el.querySelector("svg");
+  let { order } = window.getComputedStyle(svg);
+  expect(order).to.eq("1");
+  vm.$el.remove();
+  vm.$destroy();
+}
+// mock
+{
+  const Constructor = Vue.extend(Button);
+  const vm = new Constructor({
+    propsData: {
+      icon: "settings"
+    }
+  });
+  vm.$mount();
+  let spy = chai.spy(function() {});
+  vm.$on("click", spy);
+  // 希望这个函数被执行
+  let button = vm.$el;
+  button.click();
+  expect(spy).to.have.been.called();
 }
