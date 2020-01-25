65db0e84067796a4503201375bc2c0258e7bdfc4
Irelia <yaokai729@outlook.com>
2020-01-25 20:42:29 +0800

    添加四个测试用例

--- src/app.js
+++ src/app.js
@@ -15,3 +15,69 @@ new Vue({
     loading3: false
   }
 })
+
+
+import chai from 'chai'
+
+const expect = chai.expect
+// 单元测试
+{
+  const Constructor = Vue.extend(Button)
+  const vm = new Constructor({
+    propsData: {
+      icon: 'settings'
+    }
+  })
+  vm.$mount()
+  let useElement = vm.$el.querySelector('use')
+  let href = useElement.getAttribute('xlink:href')
+  expect(href).to.eq('#i-settings')
+  vm.$el.remove()
+  vm.$destroy()
+} {
+  const Constructor = Vue.extend(Button)
+  const vm = new Constructor({
+    propsData: {
+      icon: 'settings',
+      loading: true
+    }
+  })
+  vm.$mount() // 放在内存中
+  let useElement = vm.$el.querySelector('use')
+  let href = useElement.getAttribute('xlink:href')
+  expect(href).to.eq('#i-loading')
+  vm.$el.remove()
+  vm.$destroy()
+} {
+  const div = document.createElement('div')
+  document.body.appendChild(div)
+  const Constructor = Vue.extend(Button)
+  const vm = new Constructor({
+    propsData: {
+      icon: 'settings',
+    }
+  })
+  vm.$mount(div) // 放在页面中，否则css不会添加到元素上
+  let svg = vm.$el.querySelector('svg')
+  let {
+    order
+  } = window.getComputedStyle(svg)
+  expect(order).to.eq('1')
+  vm.$el.remove()
+  vm.$destroy()
+}
+// mock
+{
+  const Constructor = Vue.extend(Button)
+  const vm = new Constructor({
+    propsData: {
+      icon: "settings"
+    }
+  })
+  vm.$mount()
+  vm.$on('click', function () {
+    expect(1).to.eq(1)
+  })
+  let button = vm.$el
+  button.click()
+}
