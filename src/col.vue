5b67c482b874371a8cea90c53fb14e8b7d76bcd9
Irelia <yaokai729@outlook.com>
2020-01-29 11:51:20 +0800

    row 和 col 基本完成

--- src/col.vue
+++ src/col.vue
@@ -1,13 +1,30 @@
 <template>
-  <div class="col" :class="[`col-${span}`]">
-    <slot></slot>
+  <div
+    class="col"
+    :class="[span && `col-${span}`, offset && `offset-${offset}`]"
+    :style="{paddingLeft: gutter/2+'px', paddingRight: gutter/2 +'px'}"
+  >
+    <div style="border: 1px solid green; height:100px">
+      <slot></slot>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
+  name: "FallenCol",
   props: {
-    span: [Number, String]
+    span: {
+      type: [Number, String]
+    },
+    offset: {
+      type: [Number, String]
+    }
+  },
+  data() {
+    return {
+      gutter: 0
+    };
   }
 };
 </script>
@@ -15,15 +32,21 @@ export default {
 <style lang='scss' scoped>
 .col {
   width: 50%;
-  height: 100px;
-  background: #eee;
-  border: 1px solid #ccc;
+  // background: #eee;
+  // border: 1px solid #ccc;
 
   $class-prefix: col-; // class 前缀
   @for $n from 1 through 24 {
     // 遍历 1 到 24，$i 表示默认从 0 开始
     &.#{$class-prefix}#{$n} {
       width: ($n / 24) * 100%;
+    }
+  }
+  $class-prefix: offset-;
+  @for $n from 1 through 24 {
+    // 遍历 1 到 24，$i 表示默认从 0 开始
+    &.#{$class-prefix}#{$n} {
+      margin-left: ($n / 24) * 100%;
     }
   }
 }
--- src/row.vue
+++ src/row.vue
@@ -1,16 +1,24 @@
 <template>
-  <div class="row">
+  <div class="row" :style="{marginLeft: -gutter/2 +'px', marginRight: -gutter/2+'px'}">
     <slot></slot>
   </div>
 </template>
 
 <script>
 export default {
-  components: {},
-  data() {
-    return {};
+  name: "FallenRow",
+  props: {
+    gutter: {
+      type: [Number, String]
+    }
   },
-  methods: {}
+
+  mounted() {
+    console.log(this.$children);
+    this.$children.forEach(vm => {
+      vm.gutter = this.gutter;
+    });
+  }
 };
 </script>
 
