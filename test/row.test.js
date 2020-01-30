8e122f87f0350606fc097961a5dac9e0d46f39b4
Irelia <yaokai729@outlook.com>
2020-01-30 11:57:12 +0800

    添加 row 组件的测试用例

---
+++ test/row.test.js
@@ -0,0 +1,54 @@
+const expect = chai.expect;
+import Vue from 'vue'
+import Row from '../src/row'
+import Col from '../src/col'
+
+Vue.config.productionTip = false
+Vue.config.devtools = false
+
+describe('Row', () => {
+  // BDD 行为驱动测试 
+  it('存在.', () => {
+    expect(Row).to.exist
+  })
+  it('接受 gutter 属性', (done) => {
+    Vue.component('f-row', Row)
+    Vue.component('f-col', Col)
+    const div = document.createElement('div')
+    document.body.appendChild(div)
+    div.innerHTML = `
+    <f-row gutter="20">
+      <f-col span="12"></f-col>
+      <f-col span="12"></f-col>
+    </f-row>
+    `
+    const vm = new Vue({
+      el: div
+    })
+    setTimeout(() => {
+      const row = vm.$el.querySelector('.row')
+      expect(getComputedStyle(row).marginRight).to.eq('-10px')
+      expect(getComputedStyle(row).marginLeft).to.eq('-10px')
+      const cols = vm.$el.querySelectorAll('.col')
+      expect(getComputedStyle(cols[0]).paddingRight).to.eq('10px')
+      expect(getComputedStyle(cols[1]).paddingLeft).to.eq('10px')
+      done()
+      vm.$el.remove()
+      vm.$destroy()
+    })
+  })
+  it('接受 align 属性', () => {
+    const div = document.createElement('div')
+    document.body.appendChild(div)
+    const Constructor = Vue.extend(Row)
+    const vm = new Constructor({
+      propsData: {
+        align: 'left'
+      }
+    }).$mount(div)
+    const element = vm.$el
+    expect(getComputedStyle(element).justifyContent).to.equal('flex-start')
+    div.remove()
+    vm.$destroy()
+  })
+})
