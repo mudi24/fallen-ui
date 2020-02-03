744ae31f334354b325f25e4943f5a069868ad1c8
Irelia <yaokai729@outlook.com>
2020-02-03 11:41:19 +0800

    添加 popover 基本样式

--- src/popover.vue
+++ src/popover.vue
@@ -3,7 +3,7 @@
     <div ref="contentWrapper" class="content-wrapper" v-if="visible">
       <slot name="content"></slot>
     </div>
-    <span ref="triggerWrapper">
+    <span ref="triggerWrapper" style="display:inline-block">
       <slot></slot>
     </span>
   </div>
@@ -38,6 +38,13 @@ export default {
       ) {
         return;
       }
+      if (
+        this.$refs.contentWrapper &&
+        (this.$refs.contentWrapper === e.target ||
+          this.$refs.contentWrapper.contains(e.target))
+      ) {
+        return;
+      }
       this.close();
     },
     open() {
@@ -49,8 +56,6 @@ export default {
       });
     },
     close() {
-      console.log("关闭");
-
       this.visible = false;
       document.removeEventListener("click", this.onClickDocument);
     },
@@ -68,6 +73,8 @@ export default {
 </script>
 
 <style lang='scss' scoped>
+$border-color: #333;
+$border-radius: 4px;
 .popover {
   display: inline-block;
   vertical-align: top;
@@ -75,8 +82,32 @@ export default {
 }
 .content-wrapper {
   position: absolute;
-  border: 1px solid #000;
-  box-shadow: 0 0 3px rgba(0, 0, 0, 0.5);
+  border: 1px solid $border-color;
+  border-radius: $border-radius;
+  // box-shadow: 0 0 3px rgba(0, 0, 0, 0.5);
+  filter: drop-shadow(0 1px 1px rgba(0, 0, 0, 0.5));
   transform: translateY(-100%);
+  margin-top: -10px;
+  padding: 0.5em 1em;
+  max-width: 200px;
+  word-break: break-all;
+  &::before,
+  &::after {
+    content: "";
+    display: block;
+    border: 10px solid transparent;
+    width: 0;
+    height: 0;
+    position: absolute;
+    left: 10px;
+  }
+  &::before {
+    border-top-color: black;
+    top: 100%;
+  }
+  &::after {
+    border-top-color: white;
+    top: calc(100% - 1px);
+  }
 }
 </style>
