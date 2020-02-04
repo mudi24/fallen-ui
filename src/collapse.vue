b403af0cafd02ed14b18d7f0beb1244f96ac4775
Irelia <yaokai729@outlook.com>
2020-02-04 12:16:04 +0800

    collapse 组件基本完成

--- src/collapse.vue
+++ src/collapse.vue
@@ -14,11 +14,28 @@ export default {
       default: false
     },
     selected: {
-      type: String
+      type: Array
     }
   },
   mounted() {
     this.eventBus.$emit("update:selected", this.selected);
+    this.eventBus.$on("update:addSelected", name => {
+      let selectedCopy = JSON.parse(JSON.stringify(this.selected));
+      if (this.single) {
+        selectedCopy = [name];
+      } else {
+        selectedCopy.push(name);
+      }
+      this.$emit("update:selected", selectedCopy);
+      this.eventBus.$emit("update:selected", selectedCopy);
+    });
+    this.eventBus.$on("update:removeSelected", name => {
+      let selectedCopy = JSON.parse(JSON.stringify(this.selected));
+      let index = this.selected.indexOf(name);
+      selectedCopy.splice(index, 1);
+      this.$emit("update:selected", selectedCopy);
+      this.eventBus.$emit("update:selected", selectedCopy);
+    });
   },
   provide() {
     return {
