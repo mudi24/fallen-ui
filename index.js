b038d8b2c84ec5edd6d6b6e5557774bb97a3ab9c
Irelia <yaokai729@outlook.com>
2020-04-19 19:02:15 +0800

    index.js 添加 Icon 引入

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import Button from './src/button'
 import ButtonGroup from './src/button-group'
+import Icon from './src/icon'
 import Input from './src/input'
 import Row from './src/row'
 import Col from './src/col'
